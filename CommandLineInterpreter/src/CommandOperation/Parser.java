58e50f03e1757fbf4f13f228706a45a707372ed6
Asmaa saleh <92892342+AsmaaSal@users.noreply.github.com>
2023-08-02 16:29:22 +0300

    Update Parser.java

--- CommandLineInterpreter/src/CommandOperation/Parser.java
+++ CommandLineInterpreter/src/CommandOperation/Parser.java
@@ -1,4 +1,15 @@
 package CommandOperation;
 
 public class Parser {
+   public static void parse(String userInput) {
+        String[] tokens = userInput.split("\\s+");
+        String command = tokens[0];
+        Terminal t = new Terminal();
+        if (command.equals("clear")) {
+            t.clear();
+        } else if (command.equals("pwd")) {
+            t.currentPath();
+        }
+
+    }
 }
