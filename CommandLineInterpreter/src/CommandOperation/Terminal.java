04b2261ace76f2493b3298e87aba915966eae000
Asmaa saleh <92892342+AsmaaSal@users.noreply.github.com>
2023-08-02 15:56:20 +0300

    Update Terminal.java

--- CommandLineInterpreter/src/CommandOperation/Terminal.java
+++ CommandLineInterpreter/src/CommandOperation/Terminal.java
@@ -1,4 +1,21 @@
 package CommandOperation;
+import java.nio.file.Paths;
 
 public class Terminal {
+   public void clear() {
+
+        // Code to clear the console screen
+
+        for(int i=0;i<30;i++) {
+            System.out.println("");
+        }
+    }
+    public void currentPath() {
+    /////prints the current path.(pwd)
+        java.nio.file.Path path = Paths.get("");
+
+        // Convert the Path to a string representation of the current path
+        System.out.println( path.toAbsolutePath().toString());
+    }
+
 }
