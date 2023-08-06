b22b8ca5fde55c8c82f886e4744fa017700a4da6
Asmaa saleh <92892342+AsmaaSal@users.noreply.github.com>
2023-08-06 16:04:30 +0300

    Update Terminal.java

--- CommandLineInterpreter/src/CommandOperation/Terminal.java
+++ CommandLineInterpreter/src/CommandOperation/Terminal.java
@@ -27,4 +27,30 @@ public class Terminal {
         }
     }
 
+   public void copycontent(String source, String destination) {
+        try {
+            // Create input and output streams
+            FileInputStream inputStream = new FileInputStream(source);
+            FileOutputStream outputStream = new FileOutputStream(destination);
+
+            // Create a buffer to hold the data while copying
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            // Copy data from the source file to the destination file
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+
+            // Close the streams
+            inputStream.close();
+            outputStream.close();
+
+            System.out.println("File copied successfully.");
+        } catch (IOException e) {
+            System.err.println("Error occurred while copying the file: " + e.getMessage());
+        }
+
+    }
+
 }
